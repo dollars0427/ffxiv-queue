057756ac3371efba25d01c4389c5779fbca6b52c
Sardo Ip <sardo@pixelactionstudio.com>
2019-10-29 17:45:53 +0800

    Ready to use tk to create GUI

--- ffxivqueue.py
+++ ffxivqueue.py
@@ -3,15 +3,21 @@ import time
 import tkinter as tk
 import requests
 
-root = tk.Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+win = tk.Tk()
+screen_width = win.winfo_screenwidth()
+screen_height = win.winfo_screenheight()
 
 x1 = screen_width / 2 - 60
 y1 = screen_height / 2 - 20
 x2 = x1 + 40
 y2 = y1 + 30
 
+win.title('FF14 排队检测器')
+button=tk.Button(win, text='OK')
+button.pack() #顯示元件
+win.mainloop()
+
+"""
 if __name__ == '__main__':
     time.sleep(5)
     im = ImageGrab.grab(bbox=(x1, y1, x2, y2))
@@ -21,3 +27,4 @@ if __name__ == '__main__':
         data = {'qq': 2590519016}
         r = requests.post('http://127.0.0.1:1000', data=data, files={'queuenum.png': f})
         print(r.text)
+"""
